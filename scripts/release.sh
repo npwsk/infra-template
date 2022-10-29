c370f256f924e39675bdfe3738273382e274dd95
Nika Perfilova <91316024+npwsk@users.noreply.github.com>
2022-10-30 01:12:10 +0700

    log previous release tag in release-ci

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,5 +1,11 @@
 echo "$TAG_PATTERN"
 echo "$GITHUB_REF_NAME"
 echo "$GITHUB_ACTOR"
+
 CHANGELOG=$(git log --pretty=format:"%h %cn: %s")
 echo "$CHANGELOG"
+
+
+CURRENT_VERSION=$GITHUB_REF_NAME
+PREV_VERSION=$(git tag | sort -V | grep $TAG_PATTERN | grep -B 1 $CURRENT_VERSION | head -1)
+echo "$PREV_VERSION"
